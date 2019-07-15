902b7ea7cf7609178c293e4e7ba16bf3e316bbe6
Jonghwan Hyeon <hyeon0145@gmail.com>
2019-07-15 21:50:37 +0900

    Support UID and GID

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import asyncio
 import os
+import subprocess
 import sys
 import yaml
 
 from rfl.app import app
 from rfl.utils import merge_dict
 
+owner_exists = ('UID' in os.environ) and ('GID' in os.environ)
 
 def load_config(filename):
     if not os.path.exists(filename):
@@ -13,7 +15,7 @@ def load_config(filename):
         return
 
     with open(filename, 'r', encoding='utf-8') as input_file:
-        config = yaml.load(input_file)
+        config = yaml.full_load(input_file)
 
     default = config.pop('default')
     config['streams'] = {
@@ -23,9 +25,17 @@ def load_config(filename):
 
     return config
 
+if __name__ == '__main__':
+    if owner_exists:
+        uid, gid = int(os.environ['GID']), int(os.environ['UID'])
+        subprocess.run(['chown', f'{uid}:{gid}', '/rfl/config.yaml'])
+        subprocess.run(['chown', f'{uid}:{gid}', '/rfl/streams'])
+ 
+        os.setgid(gid)
+        os.setuid(uid)
 
-config = load_config('config.yaml')
+    config = load_config('config.yaml')
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(app(config['streams']))
-loop.close()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(app(config['streams']))
+    loop.close()
